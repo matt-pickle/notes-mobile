68a626f7beef14fc36ade9173c6dfef9a47fe708
Matt <mpickle86@gmail.com>
2021-05-08 12:00:16 -0500

    Set up navigation

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import {Text, View, StatusBar} from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import * as firebase from "firebase";
+import apiKeys from "./config.keys";
+import WelcomeScreen from "./components/WelcomeScreen";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import LoadingScreen from "./components/LoadingScreen";
+import Dashboard from "./components/Dashboard";
 import styles from "./styles.js";
 
+const Stack = createStackNavigator();
+
 export default function App() {
+  if (!firebase.apps.length) {
+    console.log('Connected with Firebase')
+    firebase.initializeApp(apiKeys.firebaseConfig);
+  }
+
   return (
-    <View>
-      <StatusBar></StatusBar>
-        <Text>Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name={'Loading'} component={LoadingScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name='Home' component={WelcomeScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name='Sign Up' component={SignUp} options={{ headerShown: false }}/>
+      <Stack.Screen name='Sign In' component={SignIn} options={{ headerShown: false }}/>
+      <Stack.Screen name={'Dashboard'} component={Dashboard} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }

bb9fa4dc9237b9dcbff02380cb679cdd02e131ff
Matt <mpickle86@gmail.com>
2021-06-06 11:57:59 -0500

    Add email verification check on login

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-import {ActivityIndicator, View} from "react-native";
+import {ActivityIndicator, View, Alert} from "react-native";
 import * as firebase from "firebase";
 import styles from "../styles/login-styles";
 
@@ -8,6 +8,14 @@ function Loading({navigation}) {
     firebase.auth().onAuthStateChanged(user => {
       if (user && user.emailVerified) {
         navigation.replace("Dashboard");
+      } else if (user && !user.emailVerified) {
+        user.sendEmailVerification();
+        Alert.alert(
+          "Unverified Email Address",
+          "A new automated message with a verification link has been sent to your email. " +
+          "Please use it to enable your Simple Notes account by verifying your email address."
+        );
+        navigation.replace("LoginScreen");
       } else {
         navigation.replace("LoginScreen");
       }
